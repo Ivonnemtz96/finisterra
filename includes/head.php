fbcaa9450aebc4b79aa16d42c434150538c94690
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-17 13:37:32 -0700

    Se agrega diseño, nuevo proyecto

---
+++ includes/head.php
@@ -0,0 +1,21 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="keywords" content="#">
+    <meta name="description" content="#">
+    <title>Finisterra</title>
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/images/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/images/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/images/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" href="assets/images/favicon.ico">
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+    <!-- Plugin -->
+    <link href="assets/css/plugin.css" rel="stylesheet">
+    <!-- Custom Stylesheet -->
+    <link href="assets/css/style.css" rel="stylesheet">
+    <!-- Custom Responsive -->
+    <link href="assets/css/responsive.css" rel="stylesheet">
+    <!-- place -->
+</head>
