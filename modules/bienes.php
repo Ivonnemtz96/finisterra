281ae49c2f26d6f32ee1db06362176cfe176f67a
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-01-24 14:05:03 -0700

    Bienes ok

---
+++ modules/bienes.php
@@ -0,0 +1,160 @@
+<!-- Start Subheader -->
+<div class="normal-bg subheader" style="background-image: url('assets/images/subheader.jpg');">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="subheader-content">
+                    <h1 class="page-title text-custom-white fw-600">¿Qué bienes valuamos?</h1>
+                    <ul class="custom-flex breadcrumb">
+                        <li><a href="index.html">Inicio</a></li>
+                        <li>Bienes valuados</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End Subheader -->
+<!-- Start Services -->
+<section class="section-padding">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-6">
+                <div class="choose-us-box style-2 mb-xl-30">
+                    <div class="top-sec">
+                        <div class="icon">
+                            <i class="flaticon-statistics text-orange"></i>
+                        </div>
+                        <h4 class="fw-600 no-margin"><a href="service-detail.html">Inmobiliario Residencial:</a></h4>
+                    </div>
+                    <p class="fw-500 mb-xl-10">
+                    <ul>
+                        <li>Vivienda aislada (Casa Habitación).</li>
+                        <li>Vivienda en condominio.</li>
+                        <li>Departamento habitacional.</li>
+                        <li>Edificio de departamentos.</li>
+                        <li>Conjunto habitacional horizontal.</li>
+                        <li>Terrenos de uso habitacional.</li>
+                        <li>Cajón de estacionamiento cubierto y descubierto.</li>
+                        <li> Entre otros.</li>
+                    </ul>
+                    </p>
+                    <!-- <a href="service-detail.html" class="fw-600 fs-14 link">Read More <i
+                            class="fas fa-long-arrow-right text-orange"></i></a> -->
+                    <i class="flaticon-statistics bg-icon"></i>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="choose-us-box style-2 mb-xl-30">
+                    <div class="top-sec">
+                        <div class="icon">
+                            <i class="flaticon-market text-orange"></i>
+                        </div>
+                        <h4 class="fw-600 no-margin"><a href="service-detail.html">Inmobiliario Comercial:</a></h4>
+                    </div>
+                    <p class="fw-500 mb-xl-10">
+                    <ul>
+                        <li>Local comercial.</li>
+                        <li>Centro comercial.</li>
+                        <li>Oficina aislada y/o Edificio de oficinas.</li>
+                        <li>Terrenos con uso comercial y/o oficinas.</li>
+                        <li>Hospital.</li>
+                        <li>Escuela.</li>
+                        <li>Agencia automotriz.</li>
+                        <li>Edificio de estacionamiento.</li>
+                        <li>Hoteles.</li>
+                        <li>Campo de golf, entre otros.</li>
+                    </ul>
+                    </p>
+
+                    <i class="flaticon-market bg-icon"></i>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="choose-us-box style-2 mb-xl-30">
+                    <div class="top-sec">
+                        <div class="icon">
+                            <i class="flaticon-investment text-orange"></i>
+                        </div>
+                        <h4 class="fw-600 no-margin"><a href="service-detail.html">Inmobiliario Industrial:</a></h4>
+                    </div>
+                    <p class="fw-500 mb-xl-10">
+                    <ul>
+                        <li>Bodega</li>
+                        <li>Nave industrial</li>
+                        <li>Parque industrial</li>
+                        <li>Terrenos con uso industrial</li>
+
+                    </ul>
+                    </p>
+                    <i class="flaticon-investment bg-icon"></i>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="choose-us-box style-2 mb-md-30">
+                    <div class="top-sec">
+                        <div class="icon">
+                            <i class="flaticon-graphic text-orange"></i>
+                        </div>
+                        <h4 class="fw-600 no-margin"><a href="service-detail.html">Agropecuario:</a></h4>
+                    </div>
+                    <p class="fw-500 mb-xl-10">
+                    <ul>
+                        <li>Predio rustico de agostadero y temporal (con o sin construcciones y/o instalaciones).</li>
+                        <li>Predio rustico de riego y frutales (con o sin construcciones y/o instalaciones).</li>
+                        <li>Predios rústicos en transición (con o sin construcciones).</li>
+                        <li>Bienes Pecuarios.</li>
+                        <li>Bienes Acuícolas</li>
+                        <li>Semovientes.</li>
+                        <li>Bienes Agroindustriales.</li>
+                    </ul>
+                    </p>
+                    <i class="flaticon-graphic bg-icon"></i>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="choose-us-box style-2 mb-md-30">
+                    <div class="top-sec">
+                        <div class="icon">
+                            <i class="flaticon-growth text-orange"></i>
+                        </div>
+                        <h4 class="fw-600 no-margin"><a href="service-detail.html">Industrial:</a></h4>
+                    </div>
+                    <p class="fw-500 mb-xl-10">
+                    <ul>
+                        <li>Maquinaria y Equipo e Industrial</li>
+                        <li>Maquinaria y equipo aislada.</li>
+                        <li>Mobiliario y equipo de oficina.</li>
+                        <li>Unidades Industriales (inmueble, maquinaria y equipo).</li>
+                        <li>Vehículo automotor y flotillas.</li>
+                        <li>Camiones y Tractocamiones.</li>
+                        <li>Maquinaria de Construcción.</li>
+                    </ul>
+                    </p>
+                    <i class="flaticon-growth bg-icon"></i>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+                <div class="choose-us-box style-2">
+                    <div class="top-sec">
+                        <div class="icon">
+                            <i class="flaticon-box text-orange"></i>
+                        </div>
+                        <h4 class="fw-600 no-margin"><a href="service-detail.html">Otro industrial:</a></h4>
+                    </div>
+                    <p class="fw-500 mb-xl-10">
+                        <ul>
+                            <li>Aeronave</li>
+                            <li>Embarcación y Artefactos Navales.</li>
+                            <li>Obra de arte.</li>
+                            <li>Joyas</li>
+                        </ul>
+                    </p>
+                    <i class="flaticon-box bg-icon"></i>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- End Services -->
