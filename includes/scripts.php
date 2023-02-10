b00558fc1cef78e28d36a022356c3a9bf4f8a5d7
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-02-10 12:30:54 -0700

    ruta absoluta

--- includes/scripts.php
+++ includes/scripts.php
@@ -1,25 +1,25 @@
 <!-- Place all Scripts Here -->
 <!-- jQuery -->
-<script src="assets/js/jquery.min.js"></script>
+<script src="/assets/js/jquery.min.js"></script>
 <!-- Popper -->
-<script src="assets/js/popper.min.js"></script>
+<script src="/assets/js/popper.min.js"></script>
 <!-- Bootstrap -->
-<script src="assets/js/bootstrap.min.js"></script>
+<script src="/assets/js/bootstrap.min.js"></script>
 <!-- Inview -->
-<script src="assets/js/jquery.inview.min.js"></script>
+<script src="/assets/js/jquery.inview.min.js"></script>
 <!-- Slick Slider -->
-<script src="assets/js/slick.min.js"></script>
+<script src="/assets/js/slick.min.js"></script>
 <!-- Nice Select -->
-<script src="assets/js/jquery.nice-select.js"></script>
+<script src="/assets/js/jquery.nice-select.js"></script>
 <!-- Range Slider -->
-<script src="assets/js/jquery-ui.js"></script>
+<script src="/assets/js/jquery-ui.js"></script>
 <!-- Morris Chart -->
-<script src="assets/js/raphael-min.js"></script>
-<script src="assets/js/morris.min.js"></script>
+<script src="/assets/js/raphael-min.js"></script>
+<script src="/assets/js/morris.min.js"></script>
 <!-- Maps -->
 <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDnd9JwZvXty-1gHZihMoFhJtCXmHfeRQg"></script>
 <!-- Custom Js -->
-<script src="assets/js/custom.js"></script>
+<script src="/assets/js/custom.js"></script>
 <!-- /Place all Scripts Here -->
 
 <script>
