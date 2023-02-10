b699900f792ed04044ddfe0fdf582b2828fca0c4
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-02-10 13:09:45 -0700

    btn de contacto

--- modules/avaluo.php
+++ modules/avaluo.php
@@ -50,6 +50,14 @@
                                     </div>
 
                                 </div>
+
+                                <div class="row justify-content-center mt-2">
+                                    <div class="col-3 ">
+                                        <a href="/contacto" class="theme-btn btn-orange">
+                                            Contactar <i class="fal fa-plus"></i>
+                                        </a>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
