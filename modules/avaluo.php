ba24120568283bc7213cef620885837d847f498d
Ivonnemtz96 <ivonnemtz96@outlook.es>
2023-02-10 12:26:18 -0700

    Se agrega más servicios de avaluos con img

--- modules/avaluo.php
+++ modules/avaluo.php
@@ -57,90 +57,171 @@
             </div>
         </div>
     </div>
-    <div class="counter-sec-2 style-2 count-section">
+    <div class="counter-sec-2 style-2 count-section p-5" style="background-color: #f3f5f7;">
         <div class="container text-center">
-            <h2>Principales Bienes que valuamos</h2>
+            <h2>Giros que valuamos</h2>
             <div class="row">
-                <div class="col-md-3 mt-5 mb-5">
+
+                <div class="col-6 col-lg-3  mt-5 mb-5 icons p-4">
+                    <h5>Marcas Patentes</h5>
+                    <img src="/assets/images/icons/marcas.png" alt="">
+                </div>
+                <div class="col-6 col-lg-3  mt-5 mb-5 icons p-4">
+                    <h5>Bodegas</h5>
+                    <img src="/assets/images/icons/bodega.png" alt="">
+                </div>
+                <div class="col-6 col-lg-3  mt-5 mb-5 icons p-4">
+                    <h5>Hoteles</h5>
+                    <img src="/assets/images/icons/hotel.png" alt="">
+                </div>
+
+                <div class="col-6 col-lg-3 mt-5 mb-5 icons p-4">
                     <h5>Aeronaves</h5>
                     <img src="/assets/images/icons/avion.png" alt="">
                 </div>
-                <div class="col-md-3  mt-5 mb-5">
+                <div class="col-6 col-lg-3  mt-5 mb-5 icons p-4">
                     <h5>Bienes y raíces</h5>
                     <img src="/assets/images/icons/bienes.png" alt="">
                 </div>
-                <div class="col-md-3  mt-5 mb-5">
+                <div class="col-6 col-lg-3  mt-5 mb-5 icons p-4">
                     <h5>Camiones</h5>
                     <img src="/assets/images/icons/camiones.png" alt="">
                 </div>
-                <div class="col-md-3  mt-5 mb-5">
+                <div class="col-6 col-lg-3  mt-5 mb-5 icons p-4">
                     <h5>Autos clásicos</h5>
                     <img src="/assets/images/icons/carro.png" alt="">
                 </div>
-                <div class="col-md-3  mt-5 mb-5">
+
+
+
+                <div class="col-6 col-lg-3  mt-5 mb-5 icons p-4">
                     <h5>Joyas</h5>
                     <img src="/assets/images/icons/collar.png" alt="">
                 </div>
-                <div class="col-md-3  mt-5 mb-5">
+                <div class="col-6 col-lg-3  mt-5 mb-5 icons p-4">
                     <h5>Embarcación</h5>
                     <img src="/assets/images/icons/crucero.png" alt="">
                 </div>
-                <div class="col-md-3  mt-5 mb-5">
-
+                <div class="col-6 col-lg-3  mt-5 mb-5 icons p-4">
                     <h5>Obras de arte</h5>
                     <img src="/assets/images/icons/cuadros.png" alt="">
                 </div>
-                <div class="col-md-3  mt-5 mb-5">
+                <div class="col-6 col-lg-3  mt-5 mb-5 icons p-4">
                     <h5>Maquinaria</h5>
                     <img src="/assets/images/icons/excavador.png" alt="">
                 </div>
-                <div class="col-md-3  mt-5 mb-5">
+
+
+
+                <div class="col-6 col-lg-3  mt-5 mb-5 icons p-4">
                     <h5>Autos de colección</h5>
-                    <img src="/assets/images/icons/mustang.png" alt="">
+                    <img src="/assets/images/icons/mustang.png?=1" alt="">
                 </div>
-                <div class="col-md-3  mt-5 mb-5">
+                <div class="col-6 col-lg-3  mt-5 mb-5 icons p-4">
                     <h5>Oficinas</h5>
                     <img src="/assets/images/icons/oficinas.png" alt="">
                 </div>
-                <div class="col-md-3  mt-5 mb-5">
+                <div class="col-6 col-lg-3  mt-5 mb-5 icons p-4">
                     <h5>Locales</h5>
                     <img src="/assets/images/icons/tienda.png" alt="">
                 </div>
-                <div class="col-md-3  mt-5 mb-5">
+                <div class="col-6 col-lg-3  mt-5 mb-5 icons p-4">
                     <h5>Tractores</h5>
                     <img src="/assets/images/icons/tractor.png" alt="">
-                </div>
-                <div class="col-md-3  mt-5 mb-5">
-
-                    <img src="/assets/images/icons/" alt="">
-                </div>
-                <div class="col-md-3  mt-5 mb-5">
-
-                    <img src="/assets/images/icons/" alt="">
-                </div>
-                <div class="col-md-3  mt-5 mb-5">
-
-                    <img src="/assets/images/icons/" alt="">
-                </div>
-                <div class="col-md-3  mt-5 mb-5">
-
-                    <img src="/assets/images/icons/" alt="">
-                </div>
-                <div class="col-md-3  mt-5 mb-5">
-
-                    <img src="/assets/images/icons/" alt="">
-                </div>
-                <div class="col-md-3  mt-5 mb-5">
-
-                </div>
-                <div class="col-md-3  mt-5 mb-5">
-
-                </div>
-                <div class="col-md-3  mt-5 mb-5">
-
                 </div>
             </div>
         </div>
     </div>
 </section>
 <!-- End About us -->
+
+<section class="especiales">
+    <div class="container">
+        <h2>Principales Bienes que valuamos</h2>
+
+        <h5>Ponemos a su disposición servicios de Avalúos Especiales como lo son:</h5>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 mt-3 mb-3">
+                    <div class="card">
+                        <img class="card-img-top" style="margin-bottom: 1.5rem;" src="/assets/images/gallery-2.jpg"
+                            alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Marcas de patentes</h5>
+                            <p class="card-text">
+                                Registra tus marcas para que tus clientes identifiquen fácilmente tus productos y
+                                servicios en el mercado.
+                            </p>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="col-md-4 mt-3 mb-3">
+                    <div class="card">
+                        <img class="card-img-top" style="margin-bottom: 1.5rem;" src="/assets/images/coleccion.jpg"
+                            alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Autos de colección</h5>
+                            <p class="card-text">
+                                Proteja su inversión en autos de coleccion con una evaluación profesional. <br><br>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mt-3 mb-3">
+                    <div class="card">
+                        <img class="card-img-top" style="margin-bottom: 1.5rem;" src="/assets/images/clasicos.jpg"
+                            alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Autos clásicos</h5>
+                            <p class="card-text">
+                                Tenemos tasadores de vehículos disponibles para ayudarlo a evaluar su colección de autos
+                                clásicos.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mt-3 mb-3">
+                    <div class="card">
+                        <img class="card-img-top" style="margin-bottom: 1.5rem;" src="/assets/images/maquinaria.jpg"
+                            alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Maquinaria y equipos</h5>
+                            <p class="card-text">
+                                Valoración de maquinaria y equipos para cada industria. estimamos la depreciación a
+                                partir de factores físicos, tecnológicos, funcionales y económicos, entre otros.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mt-3 mb-3">
+                    <div class="card">
+                        <img class="card-img-top" style="margin-bottom: 1.5rem;" src="/assets/images/camiones.jpg"
+                            alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Camiones y tractocamiones</h5>
+                            <p class="card-text">
+                                Póngase en contacto con nosotros; dependiendo del motivo de su valoración, con gusto lo
+                                ayudaremos como consultores, expertos neutrales.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 mt-3 mb-3">
+                    <div class="card">
+                        <img class="card-img-top" style="margin-bottom: 1.5rem;" src="/assets/images/casas.jpg" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">Casas habitación</h5>
+                            <p class="card-text">
+                                Proporcionamos valoraciones inmobiliarias claras e independientes que incluyen un
+                                análisis detallado de su propiedad y del mercado inmobiliario en el que se encuentra.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
